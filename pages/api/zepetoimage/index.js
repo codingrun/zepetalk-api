7eb8f936a08c38604377938a1eddd32ffc889aff
이수연 <sooyeon@isuyeon-ui-MacBookAir.local>
2020-10-10 17:58:05 +0900

    텍스트에 따라 이미지 가져오는 부분 추가

--- pages/api/zepetoimage/index.js
+++ pages/api/zepetoimage/index.js
@@ -1,3 +1,6 @@
+import ZepetoImageStore from "../../../lib/zepetoimageStore";
+import axios from "axios";
+
 export default async function zepetoimageHandler(req, res) {
   const {
     query: { id },
@@ -25,18 +28,38 @@ export default async function zepetoimageHandler(req, res) {
     }
   };
 
+  const getImageType = (score) => {
+    if (score < 0) {
+      return "negative";
+    }
+    if (score === 0) {
+      return "neutrality";
+    }
+    if (score === 0) {
+      return "positive";
+    }
+  };
   switch (method) {
     case "GET":
       // Get data from your database
+      res.status(200).json({ id, name: `User ${id}` });
+      break;
+    case "PUT":
+      // Get data from your database
+      ZepetoImageStore.writeImageData();
       res.status(200).json({ id, name: `User ${id}` });
       break;
     case "POST":
       // Update or create data in your database
       try {
         const resSentimental = await getSentimental(req.body.text);
-        console.log("resSentimental = ", resSentimental);
+        const imageType = getImageType(resSentimental.documentSentiment.score);
+        const resultImages = await ZepetoImageStore.getUserImages(
+          req.body.hashcode,
+          imageType
+        );
         //지정된 이미지 url가져오기
-        res.status(200).json(resSentimental);
+        res.status(200).json({ images: resultImages });
       } catch (error) {
         console.log("error = ", error);
         res.status(500).json({ message: error });
--- utils/firebase.js
+++ utils/firebase.js
@@ -2,13 +2,15 @@ import firebase from "firebase";
 require("firebase/auth");
 require("firebase/database");
 
-firebase.initializeApp({
-  apiKey: process.env.GA_APIKEY,
-  authDomain: "junction-hack.firebaseapp.com",
-  databaseURL: "https://junction-hack.firebaseio.com/",
-  // projectId: "<your-cloud-firestore-project>",
-  // storageBucket: "<your-storage-bucket>",
-  // messagingSenderId: "<your-sender-id>",
-});
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: process.env.GA_APIKEY,
+    authDomain: "junction-hack.firebaseapp.com",
+    databaseURL: "https://junction-hack.firebaseio.com/",
+    // projectId: "<your-cloud-firestore-project>",
+    // storageBucket: "<your-storage-bucket>",
+    // messagingSenderId: "<your-sender-id>",
+  });
+}
 
 export default firebase;
